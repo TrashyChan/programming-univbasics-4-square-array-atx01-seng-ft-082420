290265f11cb1c9c661f2adfd99d57d4ca00e3b01
John Lonetree <jlonetree@gmail.com>
2020-08-08 19:34:38 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -14,8 +14,8 @@ def square_array(new_numbers)
   array = [9,10,16,25]
   newer_array = []
   
-  while array.length.times do |index|
-    puts newer_array << (array[index] * array[index])
+  while new_numbers.length.times do |index|
+    puts newer_array << (new_numbers[index] * new_numbers[index])
   end
   
   p newer_array
